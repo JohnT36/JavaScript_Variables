7bd942a2b04028002ab7c36d7d007e3f07a9570d
John Tuck <j.tuck1336@gmail.com>
2022-06-17 12:46:38 -0400

    Finished small exercise

--- app.js
+++ app.js
@@ -4,7 +4,43 @@ console.log(
 );
 
 // Exercise 1
+var fullName = "John Tuck";
+let firstName = "John";
+let lastName = "Tuck";
+var age = 26;
+
+let greeting = "My name is " + fullName;
+let templateGreeting = `My name is ${fullName}`;
+let ageInDays = `My age in days: ${age * 365}`;
+
+
+
+
+fullname = `${firstName}  ${lastName}`;
+firstName = "JOHN";
+lastName = "TUCK";
+var age = 27;
+
+
 
 // Exercise 2
+var hobbie = "coding";
+var hobbie2 = "spending time with Family" 
+let hobbie3 = "chill and do nuttin."
+var hobbies = [hobbie, hobbie2];
+
+var story = `My favorite hobby is ${hobbie} 
+and I like ${hobbie2}`;
+
+
+
 
 // Exercise 3
+let city = "Merritt Island";
+let state = "Florida";
+ 
+let story2 = `My name is ${fullName} born and raised in ${state}
+I grew up in a small city ${city},\n where the only thing to really do around here is to ${hobbie3}
+${story}. `
+
+console.log(story2);
